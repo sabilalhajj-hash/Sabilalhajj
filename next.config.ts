41de90d1f37d6465de19aa36f6366d27f6bcffcb
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2026-01-15 19:57:28 +0100

    Landing page done + whatsapp done + Almost finished umrah page ..

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'export',      // This creates the /out folder
+  images: {
+    unoptimized: true,   // Required for static export
+  },
+  //
   
 };
 
